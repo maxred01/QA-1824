d3edc30e221dea776dcbe01e39295347e7d79374
misha <varik-1986-varik@mail.ru>
2024-10-31 21:13:26 +0300

    мой 1 коммит

--- ilia_demidenko/class_work_2/class_work_2.py
+++ ilia_demidenko/class_work_2/class_work_2.py
@@ -1,1 +1,12 @@
-print('Hello world')
+import requests
+import pytest
+import pytest_check as check
+
+def test_api_users():
+    url = 'https://reqres.in/api/users?page=2`'
+    headers = {}
+    response = requests.request('GET', url, headers=headers)
+
+    print(response.status_code)
+
+    assert response.status_code == 200
