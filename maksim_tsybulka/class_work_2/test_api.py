65fc3de9ebdcf2f6a50704288692323b7d1ee312
maxred01 <maksim.pvo@gmail.com>
2024-10-31 21:55:13 +0300

    example

---
+++ maksim_tsybulka/class_work_2/test_api.py
@@ -0,0 +1,22 @@
+import requests
+import pytest_check as check
+
+
+def test_api_users():
+    url = "https://reqres.in/api/users?page=2"
+
+    headers = {}
+
+    response = requests.request('GET', url, headers=headers)
+    data = response.json()
+
+    print(data['page'])
+    check.equal(data['page'], 4)
+
+
+    print(response.status_code)
+
+    # assert response.status_code == 200
+
+    check.equal(response.status_code, 202, f'статус код не равен 200. Статус код равен {response.status_code}')
+    print(response)
