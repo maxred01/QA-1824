d8d0a8185c7b71d48cb114e95f133a403224838a
gyatso-code <doyzz12@hotmail.com>
2024-11-01 14:30:17 +0300

    classwork02 - test.api.change

--- artemij_shulga/class_work02/test_api.py
+++ artemij_shulga/class_work02/test_api.py
@@ -1,11 +1,11 @@
 ''' test_api '''
 
-import requests
-import pytest
-import pytest_check as check
+import requests  # pylint: disable=E0401
+import pytest_check as check   # pylint: disable=E0401
 
 
 def test_api_users():
+
     url = "https://reqres.in/api/users?page=2"
 
     headers = {}
@@ -14,4 +14,6 @@ def test_api_users():
 
     print(response.status_code)
 
-    check.equal(response.status_code, 201, f'статус код не равен 200. Статус код равен {response.status_code}')
+    check.equal(response.status_code, 201,
+                f'статус код не равен 200. Статус код равен {response.status_code}')
+
