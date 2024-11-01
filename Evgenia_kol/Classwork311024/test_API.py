11af8e0335c12601968ad38aa4689d271ec0528a
Evgenia_kol <evgeniakol@gmail.com>
2024-11-01 21:17:43 +0300

    My first API test

---
+++ Evgenia_kol/Classwork311024/test_API.py
@@ -0,0 +1,13 @@
+import requests
+import pytest
+import pytest_check as check
+
+
+def test_api_users():
+    url = 'https://reqres.in/api/users/2'
+
+    headers = {}
+
+    response = requests.request('GET', url, headers=headers)
+
+    check.equal (response.status_code , 200, 'статус код равен 200')
