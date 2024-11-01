d7cde19d629a5eaa913cb1b23d28407ca9c4d503
STSLVCH <stasilevichnik1996@gmail.com>
2024-11-01 13:45:08 +0300

    Исправил ошибки 3.0

---
+++ nikita_stasilevich/classwork2_final/classwork2_final.py
@@ -0,0 +1,18 @@
+"""test api"""
+
+import requests #pylint: disable=E0401
+import pytest_check as check #pylint: disable=E0401
+
+
+def test_api_users():
+    """Function"""
+    url = "https://reqres.in/api/users/2"
+
+    headers = {}
+
+    response = requests.request("GET", url, headers=headers)
+
+    print(response.status_code)
+
+    check.equal(response.status_code, 200, f'стастус код не равен 200. '
+                                           f'Статус код равен {response.status_code}')
