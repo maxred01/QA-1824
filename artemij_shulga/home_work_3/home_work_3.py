dd1d44d53f540d88e8e045519d47014cdf679809
gyatso-code <doyzz12@hotmail.com>
2024-11-01 21:56:51 +0300

    first homework - selenium

---
+++ artemij_shulga/home_work_3/home_work_3.py
@@ -0,0 +1,11 @@
+''' first homework '''
+
+from selenium import webdriver  # pylint: disable=E0401
+
+driver = webdriver.Chrome()
+
+driver.get("https://hoster.by")
+
+assert "Example Domain" in driver.title
+
+driver.quit()
