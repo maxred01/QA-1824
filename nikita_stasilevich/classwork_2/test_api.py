8b0f747f49cdfaf17da419061983e10dad8a47ff
STSLVCH <stasilevichnik1996@gmail.com>
2024-11-01 14:07:01 +0300

    final

--- nikita_stasilevich/classwork_2/test_api.py
+++ nikita_stasilevich/classwork_2/test_api.py
@@ -4,7 +4,7 @@ import requests #pylint: disable=E0401
 import pytest_check as check #pylint: disable=E0401
 
 def test_api_users():
-    """Function"""
+    """Function test"""
     url = "https://reqres.in/api/users/2"
 
     headers = {}
@@ -14,4 +14,4 @@ def test_api_users():
     print(response.status_code)
 
     check.equal(response.status_code, 200, f'стастус код не равен 200. Статус код равен,'
-                                                   f' {response.status_code}')
+                                           f' {response.status_code}')
