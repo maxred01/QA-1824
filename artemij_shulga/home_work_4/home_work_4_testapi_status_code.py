7dd7d0ceac8664d93f98d1dc452d9b5eac5ac305
gyatso-code <doyzz12@hotmail.com>
2024-11-03 01:01:04 +0300

    test_api - status_code - fix

--- artemij_shulga/home_work_4/home_work_4_testapi_status_code.py
+++ artemij_shulga/home_work_4/home_work_4_testapi_status_code.py
@@ -18,3 +18,19 @@ def test_api_users():
 
     check.equal(response.status_code, 200,
                 f'статус код не равен 200. Статус код равен {response.status_code}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
