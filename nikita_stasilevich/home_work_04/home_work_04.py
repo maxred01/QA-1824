b8ed53e742983f71a2306076c2824b8f13373558
STSLVCH <stasilevichnik1996@gmail.com>
2024-11-02 19:53:12 +0300

    home work 4(final)

---
+++ nikita_stasilevich/home_work_04/home_work_04.py
@@ -0,0 +1,17 @@
+"""test status code"""
+
+import requests # pylint: disable=E0401
+import pytest_check as check # pylint: disable=E0401
+
+def test_api_users():
+    """Work funktions"""
+    url = "https://reqres.in/api/user/2"
+
+    headers = {}
+
+    response = requests.request('GET', url, headers=headers)
+
+    print(response.status_code)
+
+    check.equal(response.status_code, 200, f'status code is NOT 200, status code is,'
+                                           f'{response.status_code}')
