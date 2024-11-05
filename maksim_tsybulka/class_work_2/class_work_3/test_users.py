316f4ba12c809333f7d0ea82045ec929b9e39ffa
maxred01 <maksim.pvo@gmail.com>
2024-11-05 21:55:29 +0300

    example

--- maksim_tsybulka/class_work_2/class_work_3/test_users.py
+++ maksim_tsybulka/class_work_2/class_work_3/test_users.py
@@ -1,8 +1,10 @@
 import requests
 import pytest_check as check
 import pytest
+import allure
 
 
+@allure.feature('Проверка апи')
 def test_api_users():
     url = "https://reqres.in/api/users/2"
 
@@ -11,9 +13,12 @@ def test_api_users():
     response = requests.request('GET', url, headers=headers)
     data = response.json()
 
-    check.equal(response.status_code, 200, f'статус код не равен 200. Статус код равен {response.status_code}')
-    check.equal(data['data']['last_name'], 'We2121aver',
-                f'Фамилия не равна "Weaver". Фамилия равна {data["data"]["last_name"]}')
+    with allure.step('Проверка статус кода'):
+        check.equal(response.status_code, 200, f'статус код не равен 200. Статус код равен {response.status_code}')
+
+    with allure.step('Проверка фамилии'):
+        check.equal(data['data']['last_name'], 'We2121aver',
+                    f'Фамилия не равна "Weaver". Фамилия равна {data["data"]["last_name"]}')
 
 
 def test_api_post_users():
@@ -26,7 +31,6 @@ def test_api_post_users():
         "name": name,
         "job": job
     }
-
 
     headers = {}
 
