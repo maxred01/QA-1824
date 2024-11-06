5f2b874766ec4a3cf949f48bc6f0142097eebbcf
DIlia74 <demidenkoilia2001@gmail.com>
2024-11-06 15:16:27 +0300

    homework_3 done

--- ilia_demidenko/homework_4/homework_4.py
+++ ilia_demidenko/homework_4/homework_4.py
@@ -2,16 +2,11 @@ import requests
 import pytest_check as check
 
 def test_api_users():
-    url = "https://reqres.in/api/users?page=2"
+    url = "https://reqres.in/api/users/2"
     headers = {}
     response = requests.request('GET', url, headers=headers)
     check.equal(response.status_code,200)
-
-
-    # json_data = response.json()
-    # check.equal(response.json('data'),('last_name'))
-    #
-    # url = 'https://reqres.in/api/users?page=2'
-    # headers = {}
-    # response = requests.get(url, headers=headers)
-    # check.equal(json_data('data')('last_name'), 'Weaver')
+    ########################################
+    data = response.json()
+    print(data['data']['last_name'])
+    check.equal(data['data']['last_name'],'We123aver', f'surname is not "Weaver" its {response.status_code}')
