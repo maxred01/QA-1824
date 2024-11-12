fffb3c819a977f278c2bb957f0a6527b9f44195d
mike <mihailmoliavko@gmail.com>
2024-11-12 14:21:36 +0300

    {'error': 'Note: Only defined users succeed registration'} ошибка выдана сервером

---
+++ moliavko_m/moliavko_home_work/home_work_5.py
@@ -0,0 +1,38 @@
+import pytest_check as check
+import requests
+
+def test_api_register_successful():
+    url = "https://reqres.in/api/register"
+
+    id = "1"
+    name = "cerulean"
+    year = "2000"
+    color = "#98B2D1"
+    pantone_value = "15-4020"
+    headers = {}
+    email = "test1111@gmail.com"
+    password = "1234561111"
+
+    data = {
+         "id": id,
+       "name": name,
+        "year": year,
+        "color": color,
+        "pantone_value": pantone_value,
+        "email": email,
+        "password": password
+    }
+
+    response = requests.request('POST', url, data=data, headers=headers)
+    data_response = response.json()
+    print(data_response)
+
+    check.equal(data_response["id"], id)
+    check.equal(data_response["name"], name)
+    check.equal(data_response['year'], year)
+    check.equal(data_response['color'], color)
+    check.equal(data_response['pantone_value'], pantone_value)
+    check.equal(data_response['total pages'], 2)
+    check.equal(data_response['total'], 12)
+    check.equal(data_response['per_page'], 6)
+    check.equal(response.status_code, 400)
