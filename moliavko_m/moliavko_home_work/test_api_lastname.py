d022177bf21d021c1ba2229d9f76d9fdfaae80c7
maxred01 <maksim.pvo@gmail.com>
2024-11-16 21:54:13 +0300

    fux error

--- moliavko_m/moliavko_home_work/test_api_lastname.py
+++ moliavko_m/moliavko_home_work/test_api_lastname.py
@@ -5,11 +5,11 @@ import pytest_check as check
 def test_api_users():
     url = "https://reqres.in/api/users/2"
     headers = {}
-    response= requests.request('GET', url, headers=headers)
+    response = requests.request('GET', url, headers=headers)
     data = response.json()
 
-    check.equal(data['data']['last_name'],'Weaver',
-    f'фамилия не равна "Weaver", фамилия равна{data['data']['last_name']}')
+    check.equal(data['data']['last_name'], 'Weaver',
+                'фамилия не равна "Weaver"')
 
     check.equal(response.status_code, 200,
-    f'статус код не равен 200, статус код равен {response.status_code}')
+                f'статус код не равен 200, статус код равен {response.status_code}')
