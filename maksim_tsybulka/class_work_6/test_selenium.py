d77d3a117a297e44a89ade5dde28d2fd2672bd9d
maxred01 <maksim.pvo@gmail.com>
2024-11-19 21:58:44 +0300

    add example

--- maksim_tsybulka/class_work_6/test_selenium.py
+++ maksim_tsybulka/class_work_6/test_selenium.py
@@ -11,15 +11,31 @@ from selenium.webdriver.common.keys import Keys
 @allure.feature('Проверка заполнения формы')
 def test_text_box():
     driver = webdriver.Chrome()
-    driver.set_window_size(1066660, 20666660)
+    driver.maximize_window()
     driver.get("https://demoqa.com/text-box")
 
-    with allure.step('Проверка поля "userName"'):
-        input_user_name = driver.find_element(By.ID, 'userName')
-        check.is_true(input_user_name.is_displayed(), 'Инпута нет на экарне')
+    elements_form = [
+        (driver.find_element(By.ID, 'userName'), 'userName', 'Мой первый текст'),
+        (driver.find_element(By.ID, 'userEmail'), 'userEmail', 'test@test.com'),
+        (driver.find_element(By.ID, 'currentAddress'), 'currentAddress', 'Мой первый текст'),
+        (driver.find_element(By.ID, 'permanentAddress'), 'permanentAddress', 'Мой первый текст')
+    ]
 
-    with allure.step('Ввод текста в поле "userName"'):
-        input_user_name.send_keys('Мой первый текст')
+    for elements, elements_text, elements_send_keys in elements_form:
+        with allure.step(f'Поля "{elements_text}" на отображение'):
+            time.sleep(1)
+            check.is_true(elements.is_displayed())
+            elements.send_keys(elements_send_keys)
+
+    with allure.step('Нажатие на кноку Submit'):
+        time.sleep(2)
+        driver.execute_script('window.scrollBy(0, 300);')
+        driver.find_element(By.ID, 'submit').click()
+
+    with allure.step('Првоерка, что форма успешно заполнена'):
+        check.is_true(driver.find_element(By.ID, 'output').is_displayed(), 'Формы не создана')
+
+        time.sleep(5)
 
     driver.close()
     driver.quit()
