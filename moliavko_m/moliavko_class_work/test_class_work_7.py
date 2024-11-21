5a599e62b170f5c3e8eb59c1637651a7cd950e68
mike <mihailmoliavko@gmail.com>
2024-11-21 22:18:02 +0300

    допил классной работы

---
+++ moliavko_m/moliavko_class_work/test_class_work_7.py
@@ -0,0 +1,25 @@
+import time
+import allure
+import pytest_check as check
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+@allure.story('проверка сайта')
+@allure.feature('проверка чекбоксов')
+def test_check_box():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get("https://demoqa.com/checkbox")
+
+    driver.find_element(By.XPATH, "(//*[@class='rct-collapse rct-collapse-btn'])[1]").click()
+    driver.find_element(By.XPATH, "(//*[@class='rct-collapse rct-collapse-btn'])[2]").click()
+    driver.find_element(By.XPATH, "(//*[@class='rct-checkbox'])[3]").click()
+    check_box_notes = driver.find_element(By.XPATH, "//*[@id='tree-node-notes']")
+    check.is_true(check_box_notes.is_selected())
+
+    time.sleep(2)
+
+    driver.close()
+    driver.quit()
