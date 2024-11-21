cb7ea0b1fa08c1e11dfef5eaf9534f18d94b9eec
mike <mihailmoliavko@gmail.com>
2024-11-21 22:40:03 +0300

    допил классной работы

--- moliavko_m/moliavko_class_work/test_class_work_7.py
+++ moliavko_m/moliavko_class_work/test_class_work_7.py
@@ -17,9 +17,14 @@ def test_check_box():
     driver.find_element(By.XPATH, "(//*[@class='rct-collapse rct-collapse-btn'])[2]").click()
     driver.find_element(By.XPATH, "(//*[@class='rct-checkbox'])[3]").click()
     check_box_notes = driver.find_element(By.XPATH, "//*[@id='tree-node-notes']")
-    check.is_true(check_box_notes.is_selected())
+    len_elements = driver.find_elements(By.XPATH, '//input[@type="checkbox"]')
 
     time.sleep(2)
+
+    check.is_true(check_box_notes.is_selected())
+    check.is_true(len(len_elements), 6)
+
+
 
     driver.close()
     driver.quit()
