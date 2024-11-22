396e7dd1fee6071f35b552c933494a2fe0d32957
STSLVCH <stasilevichnik1996@gmail.com>
2024-11-23 00:13:41 +0300

    Homework test modal dialogs

---
+++ nikita_stasilevich/Homework_7/test_modal_dialogs.py
@@ -0,0 +1,40 @@
+import time
+import allure
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+@allure.story('test site')
+@allure.feature('test checkbox')
+def test_accordion():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get("https://demoqa.com/modal-dialogs")
+    time.sleep(1)
+    driver.execute_script('window.scrollBy(0, 300);')
+    time.sleep(1)
+    modal_click_small = driver.find_element(By.ID, 'showSmallModal')
+    modal_click_small.click()
+    time.sleep(3)
+    modal_click_small_close = driver.find_element(By.ID, 'closeSmallModal')
+    modal_click_small_close.click()
+    modal_click_small = driver.find_element(By.ID, 'showSmallModal')
+    modal_click_small.click()
+    time.sleep(2)
+    modal_click_small_close = driver.find_element(By.XPATH, ' //button[@class="close"]')
+    modal_click_small_close.click()
+    time.sleep(3)
+    modal_click_large = driver.find_element(By.ID, 'showLargeModal')
+    modal_click_large.click()
+    time.sleep(3)
+    modal_click_large_close = driver.find_element(By.ID, 'closeLargeModal')
+    modal_click_large_close.click()
+    time.sleep(3)
+    modal_click_large = driver.find_element(By.ID, 'showLargeModal')
+    modal_click_large.click()
+    time.sleep(2)
+    modal_click_large_close = driver.find_element(By.XPATH, '//button[@class="close"]')
+    modal_click_large_close.click()
+    time.sleep(2)
+    driver.close()
+    driver.quit()
+
