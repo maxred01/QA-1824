3fce06b8d66347e8f0d010a6cf6f93f6dc8dfe0a
STSLVCH <stasilevichnik1996@gmail.com>
2024-11-22 11:13:42 +0300

    Классная работа 7 Тест сайта

--- nikita_stasilevich/Classwork_7/test_is_selected.py
+++ nikita_stasilevich/Classwork_7/test_is_selected.py
@@ -3,27 +3,25 @@ import allure
 import pytest_check as check
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-@allure.story('Проверка сайта')
-@allure.feature('Проверка чекбоксов')
+
+@allure.story('test site')
+@allure.feature('test checkbox')
 def test_checkbox():
-    """Тест проверки работоспособности чекбоксов"""
+    '''Test Checkbox'''
     driver = webdriver.Chrome()
     driver.maximize_window()
-
     driver.get("https://demoqa.com/checkbox")
     check_box_home_click = driver.find_element(By.XPATH, '//*[@id="tree-node"]/ol/li/span/button')
     check_box_home_click.click()
     time.sleep(1)
-
-    check_box_home_desctop = driver.find_element(By.XPATH, '//*[@id="tree-node"]/ol/li/ol/li[1]/span/button')
-    check_box_home_desctop.click()
+    check_box_desctop_click = driver.find_element(By.XPATH, '//*[@id="tree-node"]/ol/li/ol/li[1]'                                                            '/span/button')
+    check_box_desctop_click.click()
     time.sleep(1)
-
-    check_box_home_notes = driver.find_element(By.XPATH, '//*[@id="tree-node"]/ol/li/ol/li[1]/ol/li[1]/span/label/span[1]')
-    check_box_home_notes.click()
-
-
-
-
+    check_box_notes = driver.find_element(By.XPATH, '//*[@id="tree-node"]/ol/li/ol/li[1]/ol/li[1]'
+                                                    '/span/label/span[1]')
+    check_box_notes.click()
+    time.sleep(1)
+    check_box_notes_click = driver.find_element(By.XPATH, '//*[@id="tree-node-notes"]')
+    check.is_true(check_box_notes_click.is_selected())
     driver.close()
     driver.quit()
