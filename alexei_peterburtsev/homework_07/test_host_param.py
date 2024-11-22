ff72b3716c3a5b4931806663d8f61fd26e994a52
Alexei-Peterburtsev <alexeipeterburtsev@gmail.com>
2024-11-22 11:46:52 +0300

    test_parametrize Hoster.by

---
+++ alexei_peterburtsev/homework_07/test_host_param.py
@@ -0,0 +1,26 @@
+'''testing hoster'''
+
+import requests
+import allure
+import pytest
+import pytest_check as check
+
+@allure.feature('Api tests')
+@allure.description('Test')
+@allure.tag('Api', 'Positive')
+@allure.severity(allure.severity_level.CRITICAL)
+@allure.label('owner', 'Alexei Peterburtsev')
+@allure.link('https://www.hoster.by/', name='Website')
+@allure.issue('AUTH-123')
+@allure.testcase('TMS-456')
+@pytest.mark.parametrize('url', [
+                                'service/solutions/ssl/',
+                                'service/solutions/ssl/not_exist',
+                                 ]
+                         )
+def test_url(url):
+    '''test func'''
+    respons = requests.get(f'https://www.hoster.by/{url}')
+
+    with allure.step(f'Test site {url} on status code'):
+        check.equal(respons.status_code, 200)
