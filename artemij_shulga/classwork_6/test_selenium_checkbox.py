2a625d1f3681ab74126879096ec341257cd7bc2b
gyatso-code <doyzz12@hotmail.com>
2024-11-23 02:13:04 +0300

    test_selenium_checkbox

---
+++ artemij_shulga/classwork_6/test_selenium_checkbox.py
@@ -0,0 +1,31 @@
+import time
+import allure
+import pytest_check as check
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+@allure.story('Test checkbox')
+@allure.feature('Check button')
+def test_text_box():
+    """ Testing checkbox """
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get("https://demoqa.com/checkbox")
+    check_arrow_click = driver.find_element(By.XPATH, '//*[@id="tree-node"]/ol/li/span/button')
+    check_arrow_click.click()
+    time.sleep(2)
+
+    check_arrow_two = driver.find_element(By.XPATH, '//*[@id="tree-node"]/ol/li/ol/li[1]/span/button')
+    check_arrow_two.click()
+    time.sleep(2)
+
+    check_arrow_three = driver.find_element(By.XPATH, '//*[@id="tree-node"]'
+                                                      '/ol/li/ol/li[1]/ol/li[1]/span/label/span[1]')
+    check_arrow_three.click()
+    time.sleep(2)
+
+    check_arrow_notes = driver.find_element(By.XPATH, '//*[@id="tree-node-notes"]')
+    check.is_true(check_arrow_notes.is_selected())
+
+    driver.close()
+    driver.quit()
