01e7e66df03ef439be270db7df244c8666fb72d7
STSLVCH <stasilevichnik1996@gmail.com>
2024-11-24 19:36:24 +0300

    Homework 8 Test parametrize

--- nikita_stasilevich/Homework_8/test_parametrize.py
+++ nikita_stasilevich/Homework_8/test_parametrize.py
@@ -14,7 +14,7 @@ import pytest_check as check
 @allure.issue("AUTH-123")
 @allure.testcase("TMS-456")
 @pytest.mark.parametrize('phone', [
-                                '375291808017'])
+                                '375296961077'])
 
 
 
