5cdeeee90c486398e757e819a46d16a0f3f72c07
STSLVCH <stasilevichnik1996@gmail.com>
2024-11-24 19:31:19 +0300

    Homework 8 Test parametrize

--- nikita_stasilevich/Homework_8/test_parametrize.py
+++ nikita_stasilevich/Homework_8/test_parametrize.py
@@ -60,7 +60,8 @@ def test_parametrize(phone):
         'sec-fetch-dest': 'empty',
         'sec-fetch-mode': 'cors',
         'sec-fetch-site': 'same-origin',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/131.0.0.0 Safari/537.36'
     }
 
     response = requests.request("POST", url, headers=headers, data=payload)
